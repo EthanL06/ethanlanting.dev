47bc83671637a4b95cfdb5e6318e38e3662de01c
Ethan <ethanlanting@gmail.com>
2025-01-05 17:22:28 -0600

    Remove Locomotive scroll

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,11 @@
-"use client";
-
 import About from "@/components/sections/About";
 import Contact from "@/components/sections/Contact";
 import Content from "@/components/sections/Content";
 import Footer from "@/components/sections/Footer";
 import Blob from "@/components/shared/Blob";
 import Navbar from "@/components/shared/Navbar";
-import { useEffect } from "react";
 
 export default function Home() {
-  useEffect(() => {
-    (async () => {
-      const LocomotiveScroll = (await import("locomotive-scroll")).default;
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const locomotiveScroll = new LocomotiveScroll();
-    })();
-  }, []);
-
   return (
     <div
       id="home"
